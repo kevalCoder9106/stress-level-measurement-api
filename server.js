60e6ead82242732493fea9af0f3dc5a567496d64
kevalCoder9106 <kevalcoder@gmail.com>
2022-08-14 09:27:36 +0530

    final

---
+++ server.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+app.use(express.json())
+
+const db = mongoose.connection
+const url = process.env.URL
+ 
+mongoose.connect(url,{
+    useNewurlParser: true,
+    useUnifiedTopology: true,
+})
+.then(console.log("Connected successfully!"))
+.catch(err => console.log(err))
+
+app.post('/insert', (req,res) => {
+    const { name, result } = req.body
+    
+    db.collection("users").insertOne({name:name,result:result})
+
+    res.json("success")
+})
+
+app.listen(3001)
